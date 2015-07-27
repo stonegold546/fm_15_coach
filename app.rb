9fd2a2b392b8e0a931be3279416ee35a256624c0
James Uanhoro <stonegold546@gmail.com>
2015-07-27 18:20:28 +0800

    Added nokogiri, users can now search for many coaches at once with added instructions

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'config_env'
 require 'rack-ssl-enforcer'
 require 'json'
 require 'slim/include'
+require 'nokogiri'
 require_relative 'model/coach'
 
 configure :development, :test do
@@ -16,6 +17,8 @@ end
 
 # App for Coach Star Calculator
 class FmCoachCalc < Sinatra::Base
+  enable :logging
+
   configure :production do
     use Rack::SslEnforcer
     set :session_secret, ENV['SYM_KEY']
@@ -64,6 +67,39 @@ class FmCoachCalc < Sinatra::Base
                              val: params.to_json }
     else
       slim :index, locals: { result: nil, val: nil }
+    end
+  end
+
+  get '/multi/?' do
+    slim :multi
+  end
+
+  post '/multi/?' do
+    begin
+      doc = Nokogiri::HTML(params['coach_file'][:tempfile])
+      data = doc.css('tr').map do |row|
+        row.css('td').map(&:text)
+      end
+      data.delete([])
+      result = [[]]
+      data.each_with_index do |x, idx|
+        coach = Coach.new(x[9].to_i, x[8].to_i, x[10].to_i)
+        result[idx] = [x[0]]
+        result[idx].push(coach.strength(x[3].to_i),
+                         coach.strength(x[3].to_i),
+                         coach.goalkeeping(x[4].to_i, x[6].to_i),
+                         coach.goalkeeping(x[4].to_i, x[7].to_i),
+                         coach.tactics(x[6].to_i),
+                         coach.ball_control(x[7].to_i, x[5].to_i),
+                         coach.defending(x[2].to_i, x[6].to_i),
+                         coach.attacking(x[1].to_i, x[6].to_i),
+                         coach.shooting(x[7].to_i, x[1].to_i))
+        result[idx][10..-1] = x[1..-1]
+        logger.info "RESULT: #{result[idx].length}"
+      end
+      slim :multi_result, locals: { result: result }
+    rescue => e
+      logger.error e
     end
   end
 end

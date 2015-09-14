38cb038efcbe7839b8df8a8b8c26c44ca427dd44
James Uanhoro <stonegold546@gmail.com>
2015-09-14 13:35:44 +0800

    Removed function from loop

--- public/index.js
+++ public/index.js
@@ -20,60 +20,62 @@ function copyFunction(val) {
     }
 }
 
+function getLatest() {
+    "use strict";
+    var ind = 0,
+        myResult = new XMLHttpRequest(),
+        url;
+    for (ind; ind < inputs.length; ind += 1) {
+        if (parseInt((inputs[ind].value), 10) < 1) {
+            inputs[ind].value = '1';
+        } else if (inputs[ind].value.length > 2) {
+            inputs[ind].value = inputs[ind].value.substring(0, 2);
+        } else if (parseInt((inputs[ind].value), 10) > 20) {
+            inputs[ind].value = '20';
+        }
+    }
+    url = '/?api=y'.concat(
+        '&attacking=',
+        inputs[0].value,
+        '&defending=',
+        inputs[1].value,
+        '&fitness=',
+        inputs[2].value,
+        '&goalkeepers=',
+        inputs[3].value,
+        '&mental=',
+        inputs[4].value,
+        '&tactical=',
+        inputs[5].value,
+        '&technical=',
+        inputs[6].value,
+        '&determination=',
+        inputs[7].value,
+        '&discipline=',
+        inputs[8].value,
+        '&motivating=',
+        inputs[9].value
+    );
+    myResult.open('GET', url, true);
+    myResult.send();
+    myResult.onreadystatechange = function () {
+        if (myResult.readyState === 4 && myResult.status === 200) {
+            var stars = document.getElementsByClassName('star'),
+                data = JSON.parse(myResult.responseText),
+                inner_idx = 0,
+                i;
+            for (i in data) {
+                stars[inner_idx].innerHTML = data[i];
+                inner_idx += 1;
+            }
+        }
+    };
+}
+
 var idx,
     input;
 
 for (idx = 0; idx < inputs.length; idx += 1) {
     input = inputs[idx];
-    input.addEventListener('input', function () {
-        "use strict";
-        var ind = 0,
-            myResult = new XMLHttpRequest(),
-            url;
-        for (ind; ind < inputs.length; ind += 1) {
-            if (parseInt((inputs[ind].value), 10) < 1) {
-                inputs[ind].value = '1';
-            } else if (inputs[ind].value.length > 2) {
-                inputs[ind].value = inputs[ind].value.substring(0, 2);
-            } else if (parseInt((inputs[ind].value), 10) > 20) {
-                inputs[ind].value = '20';
-            }
-        }
-        url = '/?api=y'.concat(
-            '&attacking=',
-            inputs[0].value,
-            '&defending=',
-            inputs[1].value,
-            '&fitness=',
-            inputs[2].value,
-            '&goalkeepers=',
-            inputs[3].value,
-            '&mental=',
-            inputs[4].value,
-            '&tactical=',
-            inputs[5].value,
-            '&technical=',
-            inputs[6].value,
-            '&determination=',
-            inputs[7].value,
-            '&discipline=',
-            inputs[8].value,
-            '&motivating=',
-            inputs[9].value
-        );
-        myResult.open('GET', url, true);
-        myResult.send();
-        myResult.onreadystatechange = function () {
-            if (myResult.readyState === 4 && myResult.status === 200) {
-                var stars = document.getElementsByClassName('star'),
-                    data = JSON.parse(myResult.responseText),
-                    inner_idx = 0,
-                    i;
-                for (i in data) {
-                    stars[inner_idx].innerHTML = data[i];
-                    inner_idx += 1;
-                }
-            }
-        };
-    });
+    input.addEventListener('input', getLatest);
 }
